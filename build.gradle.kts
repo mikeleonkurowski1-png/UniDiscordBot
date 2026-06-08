898b7c158624f84b7a89cee4449d3972dcf45c0d
M.Kurowski <mikeleonkurowski1@gmail.com>
2026-06-08 23:05:44 +0200

    JDA Dependency hinzugefügt und gitignore bearbeitet um den Token sicher speichern zu können.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("net.dv8tion:JDA:6.4.2") //Neuste Version stand 08.06.2026
 }
 
 tasks.test {
